d961cf1921f163db46d5881f1c32b54d5b1fc127
Brenda Vielmas <breen.vielmas@gmail.com>
2019-06-25 18:53:34 -0500

    comienzo de 3ra función, arreglos de sortdata

--- src/data.js
+++ src/data.js
@@ -1,5 +1,4 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+
 window.data = {
 	filterData : (input, condition) => {
 		return 'filterData';
@@ -8,13 +7,14 @@ window.data = {
 	  recibe tres parámetros. El primer parámetro, input, nos entrega los datos.
 	   El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de
 	   la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere
-	    ordenar de manera ascendente o descendente.*/
+			ordenar de manera ascendente o descendente.*/
+			
 	sortData : (input, sortBy, sortOrder) => {
-		let ascOrder = (a, b)=>{
+		/*let ascOrder = (a, b)=>{
 			if (a[sortBy] < b[sortBy]) {
 				return -1;
 			}
-			if (a[sortBy] > b[sortBy]) {
+			else if (a[sortBy] > b[sortBy]) {
 				return 1;
 			}
 			return 0;
@@ -23,26 +23,51 @@ window.data = {
 			if (a[sortBy] < b[sortBy]) {
 				return -1;
 			}
-			if (a[sortBy] > b[sortBy]) {
+			else if (a[sortBy] > b[sortBy]) {
 				return 1;
 			}
 			return 0;
-		};
+		};*/
 		/*sort = ordenar.
 		Si no se provee compareFunction, los elementos son ordenados convirtiéndolos
 		a strings y comparando la posición del valor Unicode de dichos strings.
 		Cuando se necesita acomodar con base a los atributos de un objeto se necesita
 		proveer compareFunction*/
 		let output;
-		if (sortOrder == "asc") {
-			output = input.sort(ascOrder);
-		}
-		if (sortOrder == "desc") {
-			output = input.sort(descOrder);
-		}
+		if (sortOrder == "Ascendente") {
+			output = input.sort((a, b) => {
+				if (a[sortBy] < b[sortBy]) {
+				return -1;
+				}
+				else if (a[sortBy] > b[sortBy]) {
+				return 1;
+				}
+				return 0;
+			});
+		};
+		if (sortOrder == "Descendente") {
+			output = input.sort((b, a) => {
+				if (a[sortBy] < b[sortBy]) {
+					return -1;
+				}
+				else if (a[sortBy] > b[sortBy]) {
+				return 1;
+				}
+				return 0;
+			});
+		};
     return output;
-  },
-  computeStats : () => {
-    return 'computeStats';
-	}
-};
+		
+},
+	computeStats : (input) => {
+		const typeOfPokemons = [POKEMON.pokemon[O], "type"];
+		const timesOfTypeOfPokemon = typeOfPokemons.reduce((counterType, typeOfPokemon) => {
+			counterType[typeOfPokemon] = (counterType[typeOfPokemon] || 0) + 1;
+		})
+		console.log(counterType);
+		return 'computeStats';
+		
+}
+
+}
+		
--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-let data = () => {
+let input = () => {
 };
 let sortBy = () => {
-	/*se necesita hacer una variable que quede en lugar de "asc" y otra para "name"
-	que elija el usuario desde html*/
-	let orderedPokemon = window.data.sortData(POKEMON.pokemon, "id", "desc");
+	const optionSelect = document.getElementById("orderOptions");
+	const formSelect = document.getElementById("orderForms");
+	const optionSelectInText = optionSelect.options[optionSelect.selectedIndex].text;
+	const formSelectInText = formSelect.options[formSelect.selectedIndex].text;
+	let orderedPokemon = window.data.sortData(POKEMON.pokemon, optionSelectInText, formSelectInText);
 	root.innerHTML = "";
-	orderedPokemon.forEach(function(pokemon) {
-  		root.innerHTML += `<img src="${pokemon.img}">`;
+	orderedPokemon.forEach(function (pokemon) {
+		root.innerHTML += `<img src="${pokemon.img}">`;
 	});
 };
 let sortOrder = () => {
-};
-
-
-document.getElementById("typeButton").addEventListener("click",sortBy);
+	root.innerHTML = "";
+	typeOfPokemon.forEach(function (pokemon) {
+		root.innerHTML += `<img src="${type}">`;
+});
+document.getElementById("sortButton").addEventListener("click", sortBy);
+}
