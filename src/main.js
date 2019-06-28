81993c935b588afdc5312731d2871183872febe1
Brenda Vielmas <breen.vielmas@gmail.com>
2019-06-27 21:11:27 -0500

    fusión de filtrar y mostrar

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 
 //Esta función es para filtrar los Pokemon
 let condition = () => {
+	const pokemonList = document.getElementById("pokemonList");
+	const optionSelect = document.getElementById("orderOptions");
+	const formSelect = document.getElementById("orderForms");
+	const optionSelectInText = optionSelect.options[optionSelect.selectedIndex].value;
+	const formSelectInText = formSelect.options[formSelect.selectedIndex].text;
 	const pokemonListFiltered = document.getElementById ("pokemonListFiltered");
-
-	
 	let firstListOfTypeOfPokemon = [];
-	
 	document.querySelectorAll('input[name="cbox"]:checked').forEach((checkbox) => { 
 
 		firstListOfTypeOfPokemon = firstListOfTypeOfPokemon.concat([checkbox.value]);
  	})
-	let filteredPokemon = window.data.filterData(POKEMON.pokemon, firstListOfTypeOfPokemon);
-	pokemonListFiltered.innerHTML += "";
+ 	let filteredPokemon = window.data.filterData(POKEMON.pokemon, firstListOfTypeOfPokemon);
+ 	let orderedPokemon = window.data.sortData(filteredPokemon, optionSelectInText, formSelectInText);
+	pokemonListFiltered.innerHTML = "";
 	filteredPokemon.forEach(function (pokemon) {
 		pokemonListFiltered.innerHTML += `
 		<div class = "cardOfPokemon" >
@@ -25,26 +28,6 @@ let condition = () => {
 	});
 };
 
-let sortBy = () => {
-	const pokemonList = document.getElementById("pokemonList");
-	const optionSelect = document.getElementById("orderOptions");
-	const formSelect = document.getElementById("orderForms");
-	const optionSelectInText = optionSelect.options[optionSelect.selectedIndex].value;
-	const formSelectInText = formSelect.options[formSelect.selectedIndex].text;
-	//console.log(optionSelectInText, formSelectInText);
-	let orderedPokemon = window.data.sortData(POKEMON.pokemon, optionSelectInText, formSelectInText, firstListOfTypeOfPokemon);
-	pokemonList.innerHTML = "";
-	orderedPokemon.forEach(function (pokemon) {
-		pokemonList.innerHTML += `
-		<div class = "cardOfPokemon" >
-		<img src="${pokemon.img}">
-		<p>Name: ${pokemon.name}</p>
-		<p>Id: ${pokemon.id}</p>
-		<p>Type: ${pokemon.type}</p>
-		<p>Weaknesses: ${pokemon.weaknesses}</p>
-		</div>`;
-	});
-};
 
 let computeStats =() => {
 	const pokemonStatistics = document.getElementById("pokemonStatistics");
@@ -57,6 +40,7 @@ let computeStats =() => {
 
 let showAllPokemons = () => {
 	let orderedPokemon = window.data.sortData(POKEMON.pokemon, "id", "Ascendente");
+	pokemonList.innerHTML = "";
 	orderedPokemon.forEach(function (pokemon) {
 		pokemonList.innerHTML += `
 		<div class = "cardOfPokemon" >
@@ -69,7 +53,6 @@ let showAllPokemons = () => {
 
 });
 }
-document.getElementById("sortButton").addEventListener("click", sortBy);
 document.getElementById("computeStatsButton").addEventListener("click", computeStats);
 document.getElementById("pokeBallBtn").addEventListener("click", condition);
 document.getElementById("pokeBallGif").addEventListener("click", showAllPokemons);
